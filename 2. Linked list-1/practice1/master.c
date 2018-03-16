1b42e523b57d17888030f6a68dea452fa432f26f
K.H Lee <beta1360@naver.com>
2018-03-16 21:55:53 +0900

    Update master.c

--- 2. Linked list-1/practice1/master.c
+++ 2. Linked list-1/practice1/master.c
@@ -1,1 +1,26 @@
 // Available after Mar 23, 2018
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node{
+  int data;
+  Node *next;
+} Node;
+
+Node *create_node(int data){
+// Description :
+//    Create new node to content data. 
+  
+}
+
+Node *search_node(Node *header, int data){
+// Description :
+//    Search the node to content data.
+
+}
+
+int main(void){
+
+  return 0; 
+}
